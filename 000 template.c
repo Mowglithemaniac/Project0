2515149acf3a21034067741fa0553606cf08da2f
No comment <mowglithemaniac@gmail.com>
2021-07-13 22:44:26 +0200

    Included a guide folder, with a general guide for commands, and an extensive guide for the Linux console

--- 000 template.c
+++ 000 template.c
@@ -1,11 +1,11 @@
 /*============================================================================
- Name        : 
+ Name        : 000 template.c
  Author      : Declined#8151 (Discord ID), s090116
- Version     :
+ Version     : 1.00
+ Date        : 11/07-2021
  Copyright   : Your copyright notice
  Description : Main program of the folder that ties everything together
  ============================================================================*/
-
 //#include "filename.h"
 
 void main(){
