c23ada8328ccfa415f4564c2bfa5d96e1e291ee7
No comment <mowglithemaniac@gmail.com>
2021-07-22 22:14:41 +0200

    Cleaned up some stuff, and added shell examples

---
+++ Shell Examples/003 bsub.sh
@@ -0,0 +1,28 @@
+#!/bin/sh 
+### General options 
+### -- specify queue -- 
+#BSUB -q hpc
+### -- set the job Name -- 
+#BSUB -J My_Application
+### -- ask for number of cores (default: 1) -- 
+#BSUB -n 4 
+### -- specify that the cores must be on the same host -- 
+#BSUB -R "span[hosts=1]"
+### -- specify that we need 2GB of memory per core/slot -- 
+#BSUB -R "rusage[mem=2GB]"
+### -- specify that we want the job to get killed if it exceeds 3 GB per core/slot -- 
+#BSUB -M 3GB
+### -- set walltime limit: hh:mm -- 
+#BSUB -W 24:00 
+### -- set the email address -- 
+# please uncomment the following line and put in your e-mail address,
+# if you want to receive e-mail notifications on a non-default address
+##BSUB -u your_email_address
+### -- send notification at start -- 
+#BSUB -B 
+### -- send notification at completion -- 
+#BSUB -N 
+### -- Specify the output and error file. %J is the job-id -- 
+### -- -o and -e mean append, -oo and -eo mean overwrite -- 
+#BSUB -o Output_%J.out 
+#BSUB -e Error_%J.err 
