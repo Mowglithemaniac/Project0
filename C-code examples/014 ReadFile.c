e5c16bdeb9280896ce46fc621868540b44b51a90
No comment <mowglithemaniac@gmail.com>
2021-09-17 08:42:44 +0200

    Added some content from the manual section so users can get a faster understanding on the options for fopen

--- C-code examples/014 ReadFile.c
+++ C-code examples/014 ReadFile.c
@@ -25,3 +25,21 @@ void main(){
     printf("\n");
 
 }
+
+/*
+Taken from the manual page for "fopen":
+
+       r      Open text file for reading.  The stream is positioned at the beginning of the file.
+
+       r+     Open for reading and writing.  The stream is positioned at the beginning of the file.
+
+       w      Truncate file to zero length or create text file for writing.  The stream is positioned at the beginning of the file.
+
+       w+     Open for reading and writing.  The file is created if it does not exist, otherwise it is truncated.  The stream is positioned at the beginning of the file.
+
+       a      Open for appending (writing at end of file).  The file is created if it does not exist.  The stream is positioned at the end of the file.
+
+       a+     Open  for  reading  and  appending (writing at end of file).  The file is created if it does not exist.  Output is always appended to the end of the file.  POSIX is silent on
+              what the initial read position is when using this mode.  For glibc, the initial file position for reading is at the beginning of the file, but for Android/BSD/MacOS, the ini‐
+              tial file position for reading is at the end of the file.
+*/
