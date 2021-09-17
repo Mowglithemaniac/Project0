8a118303169a1b090abcb8d27263229ecd5bd68a
No comment <mowglithemaniac@gmail.com>
2021-09-17 08:48:54 +0200

    Minor changes such as a comment about the manual

--- C-code examples/015 writeFile.c
+++ C-code examples/015 writeFile.c
@@ -17,7 +17,7 @@ void main(){
     char tempArray[maxArraySize];
     printf("Enter a filename that you want to write something to.\n"
            "=======================WARNING=======================\n"
-           "Take care to choose a file that does NOT already exist.\n"
+           "Choose a file that does NOT already exist!!!\n"
            "=======================WARNING=======================\n");
     /**This program will overwrite the content of any file you choose
      * therfore it is recommended to come up with a bogus name such as
@@ -37,3 +37,21 @@ void main(){
     fprintf(fp, "%s", test);
     fclose(fp);
 }
+
+/*
+Taken from the manual page for "fopen":
+
+       r      Open text file for reading.  The stream is positioned at the beginning of the file.
+
+       r+     Open for reading and writing.  The stream is positioned at the beginning of the file.
+
+       w      Truncate file to zero length or create text file for writing.  The stream is positioned at the beginning of the file.
+
+       w+     Open for reading and writing.  The file is created if it does not exist, otherwise it is truncated.  The stream is positioned at the beginning of the file.
+
+       a      Open for appending (writing at end of file).  The file is created if it does not exist.  The stream is positioned at the end of the file.
+
+       a+     Open  for  reading  and  appending (writing at end of file).  The file is created if it does not exist.  Output is always appended to the end of the file.  POSIX is silent on
+              what the initial read position is when using this mode.  For glibc, the initial file position for reading is at the beginning of the file, but for Android/BSD/MacOS, the ini‐
+              tial file position for reading is at the end of the file.
+*/
