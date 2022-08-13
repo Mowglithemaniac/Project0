1f80b18e806d98c74f18c1740f4bfec5d35c7696
No comment <mowglithemaniac@gmail.com>
2022-08-13 12:44:29 +0200

    derp

--- C-code examples/D010 DLLRead.c
+++ C-code examples/D010 DLLRead.c
@@ -4,19 +4,17 @@
  Version     : v1.00
  Copyright   : 
  Dato        : 01/06-21
- Description : Combined version of 014 and A007, to read the content of a file
-             : into nodes in a Doubly Linked List.
+ Description : Combined version of 'readfile' and 'DLL', to read the content
+             : of a file into nodes in a Doubly Linked List.
  ============================================================================*/
 
 #include <stdio.h> 
 #include <stdlib.h> 
-#include <assert.h>
 #include <string.h>
 
 //Doubly Linked List
 struct Node {
     char *data;
-    int hidden, rank, suit;
     struct Node* next; // Pointer to next node in DLL
     struct Node* prev; // Pointer to previous node in DLL
 };
@@ -34,7 +32,10 @@ void main(){
 
     FILE *fp;
     fp = fopen ("derpy.txt", "r");		// Open the file with 'read' option.	''
-    if(fp==NULL){ exit(-1);}
+    if(fp==NULL){ 
+        fprintf(stderr, "[!] Error reading from file\n");
+        exit(1);
+    }
     while(!feof(fp)){
        fgets(tempArray, maxSize, fp);
        // Uncomment this, and change the printf within printList to include a '\n' when printing each node
@@ -47,7 +48,6 @@ void main(){
             tail->data = strdup(tempArray);
         } else{
             pop();
-//        printf("%s", tempArray);
         }
         memset(tempArray, 0, sizeof(char) * maxSize);
     }
@@ -99,20 +99,20 @@ void pop(){
 
 /* Use this function to print out the current contents of memory. */
 void printList(){
-	printf("------------------------------------"	
-		   "\n------------------------------------"
-		   "\nThis function will print info from all the nodes.\n");
+    printf("------------------------------------"	
+           "\n------------------------------------"
+           "\nThis function will print info from all the nodes.\n");
     printf("Note that newline characters are NOT excluded from within nodes\n");
-	struct Node* Derpina = head;
-	int derp = 1;
-	while (1==1){
-		printf("Node data         : %s\n", Derpina->data);
-		if(Derpina->next == NULL){ // reached the last node, so stop iterating
-			break;	
-		} else {
-			Derpina = Derpina->next;
-		}	
-	}	
-	return;
+    struct Node* Derpina = head;
+    int derp = 1;
+    while (1==1){
+        printf("Node data         : %s\n", Derpina->data);
+        if(Derpina->next == NULL){ // reached the last node, so stop iterating
+            break;
+        } else {
+            Derpina = Derpina->next;
+        }
+    }	
+    return;
 }
 
